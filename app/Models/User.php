5e2a61923c63c5015e4893022ca1547d9bc199fe
Dilovar-91 <dilovar09@gmail.com>
2024-01-16 17:55:03 +0300

    yandex

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,7 +52,7 @@ class User extends Authenticatable implements FilamentUser, HasName
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->email === 'admin@accas777.ru';
+        return $this->email === 'admin@admin.com';
     }
 
     public function getFilamentName(): string
