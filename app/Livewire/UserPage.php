263d1bb34c5c0db851d8d23301fd7310c0e9ced6
Dilovar-91 <dilovar09@gmail.com>
2024-01-17 12:49:17 +0300

    yandex

--- app/Livewire/UserPage.php
+++ app/Livewire/UserPage.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 use Livewire\Component;
 
 class UserPage extends Component
 {
     public function render()
     {
+        if (!Auth::check()) {
+            // Redirect to the login page or any other desired page
+            return Redirect::to('/login');
+        }
         return view('livewire.user-page');
     }
 }
